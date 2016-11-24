87b6d72b4bb3b996df5ce509ab9c2eb173bef5a8
Jani <jani@path.com>
2016-11-24 09:44:26 +0700

    [Update] Errata

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import openScheme from './scheme-helper';
 
 window.pathnerScheme = (options) => {
     const {subject, text} = options;
-    const scheme = `path://compose/thought?subject=${encodeURIComponent(subject)},text=${encodeURIComponent(text)}`;
+    const scheme = `path://compose/thought?subject=${encodeURIComponent(subject)}&text=${encodeURIComponent(text)}`;
     openScheme(_.assignIn({scheme}, options, {dom, agent}));
 };
